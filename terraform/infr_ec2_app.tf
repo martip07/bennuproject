3d2d2be0e1583813e9def472326e4dd95483babc
Joseph <josephpaz@martip07.me>
2019-12-06 23:24:04 -0500

    Pruebas usando tempkey y no una variable de entorno en tf

--- terraform/infr_ec2_app.tf
+++ terraform/infr_ec2_app.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "uat-appserver" {
       type = "ssh"
       user = "ubuntu"
       host = self.public_ip
-      private_key = "${var.ec2_key_priv}"
+      private_key = "${file("tempkey")}"
     }
   }
 
